267a0c06a7847090341571de6a206b76826f59af
Mark Mossberg <mark.mossberg@gmail.com>
2014-11-21 01:35:42 -0500

    Refactor receiver
    Instead of taking data and checksum as separate inputs, take it all as
    one "data" input, representing the data + checksum concatenation.

--- receiver.v
+++ receiver.v
@@ -1,13 +1,14 @@
-module receiver(data, checksum, ok);
+`timescale 1 ns / 1 ns
+
+module receiver(data, ok);
 
 /* Args */
-input [31:0] data;
-input [15:0] checksum;
+input [47:0] data;
 output ok;
 
 wire [15:0] calculated;
 
-inet_chksum C(data, calculated);
+inet_chksum C(data[47:16], calculated);
 
 assign ok = checksum == calculated;
 
